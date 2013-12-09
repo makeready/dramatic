776845ecb214b47fdab70500a9450f5344b4ceca
makeready <shotsinthedark@gmail.com>
2013-12-09 14:13:19 -0500

    logging in now saves the token and secret to the user object

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['TWITTER_REST_API1'], ENV['TWITTER_REST_API2']
+  provider :twitter, ENV['TWITTER_REST_API1'], ENV['TWITTER_REST_API2'],
+    {
+      
+    }
 end
