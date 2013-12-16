cce3d2fb0d637891f04124f3800901f0c8046dbf
make ready <shotsinthedark@gmail.com>
2013-12-16 16:20:56 -0500

    enabled demo mode (change this in config/omniauth.rb)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['TWITTER_REST_API1'], ENV['TWITTER_REST_API2'],
     {
-      
+      authorize_params: {force_login: 'true'} #DEMO MODE DEMO MODE DEMO MODE
     }
 end
